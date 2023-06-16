c32e67ae730b376e274619729d1e8ca9669a4e06
jagadeeshp <jagadeeshpenupothu487@gmail.com>
2023-06-16 15:59:26 +0530

    haradhat installed and name_age.sol and .js files added

---
+++ contracts/name_age.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+contract myContract{
+    string private name;
+    uint private age;
+    function setName(string memory _name) public {
+        name = _name;
+    }
+    function getName( ) public view returns(string memory){
+        return name;
+    }
+    function setAge(uint _age) public  {
+        age = _age;
+    }
+    function getAge() public view returns(uint) {
+        return age;
+    }
+}
